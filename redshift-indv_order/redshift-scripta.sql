8cd912b45ee51f033ef404ecf9a865843ec06e88
Vladimir <61718705+vladimir-zivkovic-nbcuni@users.noreply.github.com>
2020-03-27 16:40:08 -0400

    Update redshift-scripta.sql

--- redshift-indv_order/redshift-scripta.sql
+++ redshift-indv_order/redshift-scripta.sql
@@ -1,1 +1,30 @@
-#
+TRUNCATE TABLE  jostens_stage.sales_rep ;
+​
+COPY jostens_stage.sales_rep
+FROM 's3://jostens-data-dev-analysis/sales_rep/'
+FORMAT AS PARQUET
+IAM_ROLE 'arn:aws:iam::147913565791:role/redshift-iam-role-s3-glue-athena' ;
+​
+DROP TABLE IF EXISTS jostens.sales_rep; 
+​
+CREATE TABLE jostens.sales_rep
+SORTKEY (salesrep_number) 
+AS 
+SELECT  *
+FROM  jostens_stage.sales_rep  ;
+​
+​
+/*
+SELECT *
+FROM   jostens.sales_rep 
+LIMIT  200 ;
+​
+SELECT COUNT(*)
+FROM   jostens.sales_rep ;
+​
+SELECT salesrep_number
+      ,COUNT(*)
+FROM  jostens.sales_rep
+GROUP BY salesrep_number
+HAVING COUNT(*) > 1 ;
+*/
