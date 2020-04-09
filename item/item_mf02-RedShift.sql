9c78e66c97a7eb414bb0f76ac52c6f2dee374d08
Jelena <kocicjelena@gmail.com>
2020-04-09 17:55:49 +0200

    mf02

--- item/item_mf02-RedShift.sql
+++ item/item_mf02-RedShift.sql
@@ -8,68 +8,35 @@ Select
 from stv_recents 
 where status='Running';
 SELECT pg_terminate_backend(18709);
-DROP TABLE jostens_stage.item_mf01;
-CREATE TABLE jostens_stage.item_mf01(
+DROP TABLE jostens_stage.item_mf02;
+CREATE TABLE jostens_stage.item_mf02(
     inventory_item_id bigint, 
-  organization_id bigint, 
-  cogs_code_combination_id bigint, 
-  sales_code_combination_id bigint, 
-  expense_code_combination_id bigint, 
-  organization_code varchar(3), 
-  organization_name varchar(60), 
   item_number varchar(50), 
-  std_lot_size bigint, 
-  description varchar(240), 
-  item_type varchar(80), 
-  item_status varchar(10), 
-  unit_weight bigint, 
-  weight_uom_code varchar(10), 
-  inventory_category varchar(50), 
-  purchasing_category varchar(50), 
-  gl_product_category varchar(50), 
-  prod_class_category varchar(50), 
-  prod_sub_category varchar(50), 
-  purchasing_sub_class varchar(40),
-  STRUCTURE_NAME varchar(30), 
-  primary_unit_of_measure varchar(25), 
-  generic_flag varchar(1), 
-  costing_enabled_flag varchar(1), 
-  inventory_asset_flag varchar(1), 
-  dropship_flag varchar(1), 
-  planning_make_buy varchar(5), 
-  material_cost bigint, 
-  resource_cost bigint, 
-  outside_processing_cost bigint, 
-  material_overhead_cost bigint, 
-  overhead_cost bigint, 
-  item_cost bigint, 
-  descriptor_invoice_usage varchar(240), 
-  item_type_id bigint, 
-  shippable_item_flag varchar(1), 
-  min_minmax_quantity bigint, 
-  max_minmax_quantity bigint, 
-  lot_control_code bigint, 
-  full_lead_time bigint
+  structure_name varchar(50), 
+  segment_name varchar(50), 
+  segment_value varchar(50), 
+  segment_number varchar(50), 
+  descriptor_id bigint
   );
-TRUNCATE TABLE  jostens_stage.item_mf01 ;
+TRUNCATE TABLE  jostens_stage.item_mf02 ;
 ​
-COPY jostens_stage.item_mf01
-FROM 's3://jostens-data-dev-analysis/item_mf01/'
+COPY jostens_stage.item_mf02
+FROM 's3://jostens-data-dev-analysis/item_mf02/'
 FORMAT AS PARQUET
 IAM_ROLE 'arn:aws:iam::147913565791:role/redshift-iam-role-s3-glue-athena' ;
 ​
-SELECT COUNT(*) FROM jostens_stage.item_mf01;
-SELECT * FROM jostens_stage.item_mf01;
-DROP TABLE IF EXISTS jostens.item_mf01 CASCADE; 
+SELECT COUNT(*) FROM jostens_stage.item_mf02;
+SELECT * FROM jostens_stage.item_mf02;
+DROP TABLE IF EXISTS jostens.item_mf02 CASCADE; 
 ​
-CREATE TABLE jostens.item_mf01
+CREATE TABLE jostens.item_mf02
 SORTKEY (inventory_item_id) 
 AS 
 SELECT  *
-FROM  jostens_stage.item_mf01;
+FROM  jostens_stage.item_mf02;
 ​
-select * from jostens.item_mf01 limit 5;
-select count(*) from jostens.item_mf01;
+select * from jostens.item_mf02 limit 5;
+select count(*) from jostens.item_mf02;
 
 --data type
-select pg_get_cols ('jostens.item_mf01');
+select pg_get_cols ('jostens.item_mf02');
